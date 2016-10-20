53a20fe45d27ce567e595fce288af84cad79a3cb
denis clot <clot@univ-lyon1.fr>
2016-10-20 12:02:26 +0200

    ACP avec ade4

---
+++ Analyse.R
@@ -0,0 +1,5 @@
+source('charg.R')
+
+source('ACP.R')
+
+dudi.pca(D,scannf=FALSE,nf=3)->acp
--- charg.R
+++ charg.R
@@ -4,5 +4,8 @@ read.table('data.txt')->D
 read.table('ind.txt')->I
 read.table('var.txt')->V
 
-names(D)<-V
 rownames(D)<-I
+names(D)<-V
+
+summary(D)
+
